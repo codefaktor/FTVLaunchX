b97df56725670760cdae4c1c83c0183e9358d0c0
Erik Abele <ErikAbele@users.noreply.github.com>
2020-01-17 16:10:50 +0100

    Add helper method to get all installed apps.

---
+++ app/src/main/java/de/codefaktor/ftvlaunchx/Utilities.java
@@ -0,0 +1,55 @@
+/*
+ * ### FTVLaunchX: Utilities
+ *
+ * Copyright © 2020 [Erik Abele](http://www.codefaktor.de/).
+ *
+ * Licensed under the
+ * [Apache License 2.0](http://www.apache.org/licenses/LICENSE-2.0).
+ */
+
+package de.codefaktor.ftvlaunchx;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+import de.codefaktor.ftvlaunchx.models.AppInfo;
+
+/**
+ * Provides several utilities.
+ */
+
+public class Utilities {
+	/*
+	 * Gets all installed non-system applications.
+	 *
+	 * @param ctx Context of application.
+	 * @return    A sorted list of all installed non-system applications
+	 *            in the form of AppInfo models.
+	 */
+
+	public static List<AppInfo> getInstalledApplications(Context ctx) {
+		PackageManager pm = ctx.getPackageManager();
+
+		List<ApplicationInfo> installedApps = pm.getInstalledApplications(0);
+
+		Collections.sort(installedApps,
+						 new ApplicationInfo.DisplayNameComparator(pm));
+
+		List<AppInfo> appInfos = new ArrayList<AppInfo>();
+
+		for (ApplicationInfo installedApp : installedApps) {
+			AppInfo app = new AppInfo(ctx, installedApp);
+
+			if (!app.isSystemApp()) {
+				appInfos.add(app);
+			}
+		}
+
+		return appInfos;
+	}
+}
