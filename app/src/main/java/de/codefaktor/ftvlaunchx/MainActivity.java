bb684d6370ff6220494b01e59fd5386972427c9a
Erik Abele <ErikAbele@users.noreply.github.com>
2020-01-17 16:34:10 +0100

    Add dropdown menu to select launcher app.

--- app/src/main/java/de/codefaktor/ftvlaunchx/MainActivity.java
+++ app/src/main/java/de/codefaktor/ftvlaunchx/MainActivity.java
@@ -9,16 +9,29 @@
 
 package de.codefaktor.ftvlaunchx;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import de.codefaktor.ftvlaunchx.models.AppInfo;
+import de.codefaktor.ftvlaunchx.ui.AppsAdapter;
+
 /**
- * Implements the main activity of FTVLaunchX.
+ * Implements the main activity of the application.
  */
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements OnItemSelectedListener {
 	private static final String TAG = "MainActivity";
+
+	private Spinner appsSpinner;
+	private AppsAdapter appsAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +44,36 @@ public class MainActivity extends Activity {
 
 		tvTitle.setText(getString(R.string.app_name) + " " +
 						getString(R.string.app_version));
+
+		appsSpinner = (Spinner) findViewById(R.id.sp_apps);
+		appsSpinner.setOnItemSelectedListener(this);
+
+		List<AppInfo> appInfos = Utilities.getInstalledApplications(this);
+
+		appsAdapter = new AppsAdapter(this, appInfos);
+		appsSpinner.setAdapter(appsAdapter);
+
+		String launcherPackage = Preferences.getLauncherPackage(this);
+		appsSpinner.setSelection(appsAdapter.getPosition(launcherPackage));
 	}
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
 		Log.d(TAG, "onDestroy()");
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent,
+							   View view, int position, long id) {
+		AppInfo appInfo = appsAdapter.getAppInfo(position);
+
+		if (appInfo != null) {
+			Preferences.setLauncherPackage(this, appInfo.getPackageName());
+		}
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> parent) {
 	}
 }
