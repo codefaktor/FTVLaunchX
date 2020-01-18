2a5fa68bf8dea4e3e5aa7313f476a843bf1aefec
Erik Abele <ErikAbele@users.noreply.github.com>
2020-01-18 14:40:05 +0100

    Add helper methods to launch the selected app.

--- app/src/main/java/de/codefaktor/ftvlaunchx/Utilities.java
+++ app/src/main/java/de/codefaktor/ftvlaunchx/Utilities.java
@@ -13,7 +13,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 
@@ -24,6 +26,8 @@ import de.codefaktor.ftvlaunchx.models.AppInfo;
  */
 
 public class Utilities {
+	private static long lastLaunch = 0;
+
 	/*
 	 * Determines whether the given permission has been granted.
 	 *
@@ -68,5 +72,70 @@ public class Utilities {
 		}
 
 		return appInfos;
+	}
+
+	/*
+	 * Gets the launch intent for a package, or, if not available (anymore)
+	 * the launch intent for the application itself.
+	 *
+	 * @param ctx Context of application.
+	 * @param pkg Name of package. Passing null will get the
+	 *            launch intent for the application itself.
+	 * @return    The launch intent for the package.
+	 */
+
+	public static Intent getLaunchIntentForPackage(Context ctx, String pkg) {
+		PackageManager pm = ctx.getPackageManager();
+
+		if (pkg == null) {
+			pkg = ctx.getApplicationContext().getPackageName();
+		}
+
+		Intent intent = pm.getLeanbackLaunchIntentForPackage(pkg);
+
+		if (intent == null) {
+			intent = pm.getLaunchIntentForPackage(pkg);
+		}
+
+		if (intent == null) {
+			intent = getLaunchIntentForPackage(ctx, null);
+		}
+
+		return intent;
+	}
+
+	/*
+	 * Launches the default activity of the given package.
+	 *
+	 * @param ctx Context of application.
+	 * @param pkg Name of package to launch.
+	 */
+
+	public static void launchPackage(Context ctx, String pkg) {
+		long time = System.currentTimeMillis();
+
+		if ((time - lastLaunch) < 200) {
+			return;
+		}
+
+		lastLaunch = time;
+
+		Intent lIntent = getLaunchIntentForPackage(ctx, pkg);
+
+		lIntent.setFlags(
+			Intent.FLAG_ACTIVITY_CLEAR_TOP |
+			Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS |
+			Intent.FLAG_ACTIVITY_NEW_TASK |
+			Intent.FLAG_ACTIVITY_REORDER_TO_FRONT |
+			Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED
+		);
+
+		PendingIntent pIntent = PendingIntent.getActivity(ctx, 0,
+														  lIntent, 0);
+		try {
+			pIntent.send();
+		} catch (PendingIntent.CanceledException e) {
+			e.printStackTrace();
+		}
 	}
 }
